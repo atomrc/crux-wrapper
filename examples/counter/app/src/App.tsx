a5cc2d34322c9a9da44bbd6430e4b2bff08acb5c
Thomas Belin <me@thomasbelin.fr>
2025-06-06 15:37:54 +0200

    reorganize for crux-wrapper and crux-react

--- examples/counter/app/src/App.tsx
+++ examples/counter/app/src/App.tsx
@@ -1,47 +1,42 @@
-import { wrapCrux, is } from 'react-crux';
-import init, * as core  from 'shared';
-import { ViewModel, Request, EventVariantIncrement, EffectVariantRender, Effect, EventVariantDecrement }  from 'counter_types/types/counter_types';
-import { BincodeSerializer, BincodeDeserializer }  from 'counter_types/bincode/mod';
-import { useState } from 'react';
-
-  const serializer = {
-    serialize(entity: {
-        serialize: (serializer: BincodeSerializer) => void;
-    }): Uint8Array {
-        const serializer = new BincodeSerializer();
-        entity.serialize(serializer);
-        return serializer.getBytes();
-    },
-
-    deserializeEffects(bytes: Uint8Array): Request[] {
-        const deserializer = new BincodeDeserializer(bytes);
-        const len = deserializer.deserializeLen();
-        const requests: Request[] = [];
-        for (let i = 0; i < len; i++) {
-            const request = Request.deserialize(deserializer);
-            requests.push(request);
-        }
-        return requests;
-    },
-
-    deserializeView(bytes: Uint8Array): ViewModel {
-        return ViewModel.deserialize(new BincodeDeserializer(bytes));
-    },
-};
+import { wrapCrux, is } from "crux-wrapper";
+import init, * as core from "shared";
+import {
+  ViewModel,
+  EventVariantIncrement,
+  EffectVariantRender,
+  Effect,
+  EventVariantDecrement,
+} from "counter_types/types/counter_types";
+import { useState } from "react";
+import { serializer } from "./serializer";
 
 export function App() {
-  const [state, setState] = useState("");
-  const handleEffect = async (id: number, effect: Effect, view: () => Promise<ViewModel>) => {
+  const [state, setState] = useState(0);
+  const handleEffect = async (
+    id: number,
+    effect: Effect,
+    view: () => Promise<ViewModel>,
+  ) => {
     switch (true) {
       case is(effect, EffectVariantRender): {
         let v = await view();
-        setState(v.count)
+        setState(Number(v.count));
         return undefined;
       }
     }
     return undefined;
   };
 
-  const api  = wrapCrux(init, core, handleEffect, serializer);
-  return <div><button onClick={() => api.sendEvent(new EventVariantIncrement()) }>+</button> <button onClick={() => api.sendEvent(new EventVariantDecrement()) }>-</button> {state}</div>
+  const api = wrapCrux(init, core, handleEffect, serializer);
+  return (
+    <div>
+      <button onClick={() => api.sendEvent(new EventVariantIncrement())}>
+        +
+      </button>{" "}
+      <button onClick={() => api.sendEvent(new EventVariantDecrement())}>
+        -
+      </button>{" "}
+      {state}
+    </div>
+  );
 }
