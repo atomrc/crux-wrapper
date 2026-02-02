ab90487f274130677d2e96f798406841cbe6beac
Thomas Belin <thomas@atomrc.dev>
2026-02-02 10:42:46 +0100

    chore: Fix d.ts and .js file cleanup (#21)

--- examples/counter/core/typegen.sh
+++ examples/counter/core/typegen.sh
@@ -1,4 +1,4 @@
 cargo run --package core --bin crux_cli --features cli
 
 # Remove js and d.ts files that could cause build problems
-find ./types -name "*.d.ts" -o -name "*.js" -type f -delete
+find ./types -name "*.d.ts" -delete -o -name "*.js" -type f -delete
