be4880473bc21f367b50ca818a71defe100902f4
Thomas Belin <me@thomasbelin.fr>
2026-02-13 10:51:08 +0100

    chore: upgrade to crux 0.17.0-rc2

--- examples/counter/core/src/app.rs
+++ examples/counter/core/src/app.rs
@@ -1,8 +1,8 @@
 use crux_core::{
+    Command,
     capability::Operation,
     macros::effect,
-    render::{render, RenderOperation},
-    Command,
+    render::{RenderOperation, render},
 };
 use facet::Facet;
 use futures_util::StreamExt;
@@ -68,10 +68,9 @@ impl crux_core::App for App {
     type Model = Model;
     type Event = Event;
     type ViewModel = ViewModel;
-    type Capabilities = ();
     type Effect = Effect;
 
-    fn update(&self, msg: Event, model: &mut Model, _caps: &()) -> Command<Effect, Event> {
+    fn update(&self, msg: Event, model: &mut Model) -> Command<Effect, Event> {
         match msg {
             Event::StartWatch => Command::new(|ctx| async move {
                 let mut stream = ctx.stream_from_shell(StreamOperation::Start);
